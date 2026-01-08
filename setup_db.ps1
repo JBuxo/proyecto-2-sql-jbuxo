1886efeb4f0a588445067e27c5a3b17577b493c8
Jose Buxo <josebuxojr@gmail.com>
2026-01-08 20:56:38 +0100

    fix a pasos de script

--- setup_db.ps1
+++ setup_db.ps1
@@ -2,6 +2,7 @@
 $sqlite = ".\sqlite3.exe"
 $db = ".\sales.db"
 $schema = ".\01_schema.sql"
+$dataSql = ".\02_data.sql"
 $csv = ".\sales_data.csv"
 
 # crear db y schema
@@ -11,6 +12,9 @@ Write-Output "Creating database and applying schema..."
 # importar csv
 Write-Output "Importing CSV data into raw_sales table..."
 
+# Inserts
+Write-Output "Running data transformation and inserts (02_data.sql)..."
+& $sqlite $db ".read $dataSql"
 
 $importCommands = @"
 .mode csv
